1d392579da68c226e4ed85910d32b1f8b7783f05
Yazdani <yaz4dani@gmail.com>
2019-07-19 14:49:53 +0600

    second

--- lib/DetalPage/PopularDetails.dart
+++ lib/DetalPage/PopularDetails.dart
@@ -24,7 +24,72 @@ class _PopularDetailsState extends State<PopularDetails> {
       body: new ListView(
         children: <Widget>[
 
+          new Container(
+            margin: EdgeInsets.all(10.0),
+            child: new Card(
+              elevation: 10.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(15.0)
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15.0),
+                child: new Image.network(widget.snapshot.data["pic"],
+                height: 200.0,
+                fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          new SizedBox(height: 6.0,),
 
+          Card(
+            elevation: 10.0,
+            margin: EdgeInsets.all(6.0),
+            child: new Container(
+              margin: EdgeInsets.all(10.0),
+              child: new Column(
+                children: <Widget>[
+
+                  new Container(
+                    padding: EdgeInsets.all(6.0),
+                    child: new Row(
+                      children: <Widget>[
+                        new Container(
+                          child: new CircleAvatar(
+                            child: new Text(widget.snapshot.data["title"][0]),
+                            backgroundColor: Colors.green,
+                            foregroundColor: Colors.white,
+                          ),
+                        ),
+                        new SizedBox(width: 5.0,),
+                        new Container(
+                          child: new Text(widget.snapshot.data["title"],
+                          style: TextStyle(
+                            fontSize: 21.0,
+                            color: Colors.green
+                          ),
+                          ),
+                        )
+
+
+                      ],
+                    ),
+                  ),
+                  new SizedBox(height: 5.0,),
+
+                  new Text(widget.snapshot.data["des"],
+                  style: TextStyle(
+                    fontSize: 17.0,
+                    color: Colors.black
+                  ),
+                  )
+
+
+
+                ],
+              ),
+            ),
+          )
 
         ],
       ),
